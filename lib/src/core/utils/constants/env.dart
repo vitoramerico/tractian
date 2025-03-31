ab847742b11ae380b8a1aa15c8fa82069e4ed47b
Vitor Hugo Américo <vitor_americo@hotmail.com>
2025-03-31 11:48:47 -0300

    feat: Add cache TTL configuration to environment variables and update AppEnv to retrieve them

--- lib/src/core/utils/constants/env.dart
+++ lib/src/core/utils/constants/env.dart
@@ -1,7 +1,13 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 abstract final class AppEnv {
-  static String get baseUrl => _get('BASE_URL');
+  static String _getString(String key) => dotenv.get(key, fallback: '');
+  static int _getInt(String key) => dotenv.getInt(key, fallback: 0);
 
-  static String _get(String key) => dotenv.get(key, fallback: '');
+  static String get baseUrl => _getString('BASE_URL');
+  static int get companiesCacheTtlMinutes =>
+      _getInt('COMPANIES_CACHE_TTL_MINUTES');
+  static int get assetsCacheTtlMinutes => _getInt('ASSETS_CACHE_TTL_MINUTES');
+  static int get locationsCacheTtlMinutes =>
+      _getInt('LOCATIONS_CACHE_TTL_MINUTES');
 }
