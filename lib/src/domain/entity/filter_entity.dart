5b7eeed14a2402e6d7d4229a0627fcb605455a51
Vitor Hugo Américo <vitor_americo@hotmail.com>
2025-04-01 00:38:11 -0300

    feat: Implement equality and hashCode methods for FilterEntity

--- lib/src/domain/entity/filter_entity.dart
+++ lib/src/domain/entity/filter_entity.dart
@@ -24,4 +24,22 @@ class FilterEntity {
       status: status ?? this.status,
     );
   }
+
+  @override
+  bool operator ==(covariant FilterEntity other) {
+    if (identical(this, other)) return true;
+
+    return other.companyId == companyId &&
+        other.query == query &&
+        other.sensorType == sensorType &&
+        other.status == status;
+  }
+
+  @override
+  int get hashCode {
+    return companyId.hashCode ^
+        query.hashCode ^
+        sensorType.hashCode ^
+        status.hashCode;
+  }
 }
